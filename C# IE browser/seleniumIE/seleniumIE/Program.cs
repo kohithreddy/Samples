ec3badb24753f3e8c17048c1966d526027c0bc1b
kohith Reddy <kohith@microsoft.com>
2021-08-26 23:50:22 +0530

    Selenium IE code

---
+++ C# IE browser/seleniumIE/seleniumIE/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using System;
+
+
+namespace seleniumIE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Navigate().GoToUrl("https://google.com");
+            driver.FindElement(By.Name("q")).SendKeys("Kohith Reddy");
+            driver.Quit();
+            driver.Close();
+         }
+    }
+}
